1b62ad1af232648787042750dae726685c6b6d35
Ton Cuong <ttquoccuong@gmail.com>
2022-09-27 16:18:50 +0700

    fixed typo for right check

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -8,7 +8,7 @@
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
         <li><a href="{{ route('home.index') }}" class="nav-link px-2 text-white">Home</a></li>
         @auth
-          @role('Admin')
+          @role('admin')
           <li><a href="{{ route('users.index') }}" class="nav-link px-2 text-white">Users</a></li>
           <li><a href="{{ route('roles.index') }}" class="nav-link px-2 text-white">Roles</a></li>
           @endrole
